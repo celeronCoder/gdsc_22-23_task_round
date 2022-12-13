af0deb5d5902e2d150f8b4aa30401ca3da9b7fc4
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-12-13 14:43:46 +0530

    hero section done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 
 export default function App() {
@@ -6,12 +7,13 @@ export default function App() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap"
+        href="https://fonts.googleapis.com/css2?&familiy=Space+Grotesk&family=Work+Sans&display=swap"
         rel="stylesheet"
       ></link>
       <div className="w-full min-h-screen flex items-center justify-center bg-[url('/images/background.jpg')] ">
         <div className="w-full min-h-screen bg-white text-white bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10">
           <Navbar />
+          <Hero />
         </div>
       </div>
     </>
---
+++ src/components/Hero.tsx
@@ -0,0 +1,30 @@
+import { AiOutlineRetweet } from "react-icons/ai";
+
+export function Hero() {
+  return (
+    <div className="mt-20 flex flex-col gap-20">
+      <div className="w-full items-center flex justify-center">
+        <div className="max-w-2xl flex flex-col justify-center gap-10 text-center">
+          <p className="hero text-6xl font-extrabold">
+            Collect and sell super rate{" "}
+            <span className="text-yellow-200">NFTs</span>
+          </p>
+          <p>
+            The world's first and largest digital marketplace for crypto
+            collectibles and non-figurable tokens (NFTs).
+          </p>
+        </div>
+      </div>
+      <div className="w-full flex items-center justify-center">
+        <div className="flex items-center justify-between px-6 py-4 text-xl rounded-full max-w-sm text-black font-bold bg-gradient-to-r from-orange-400 to-indigo-400 w-full">
+          <p>Explore Marketplace</p>
+          <div className="p-3 bg-yellow-200 rounded-full font-extrabold">
+            <div className="p-1 border-2 rounded-full border-black">
+              <AiOutlineRetweet size={20} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
