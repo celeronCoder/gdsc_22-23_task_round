06b13743f117790420072f0a8842e16268a806c3
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-12-13 20:17:59 +0530

    added styles to hero > btn

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,11 +16,11 @@ export function Hero() {
         </div>
       </div>
       <div className="w-full flex items-center justify-center">
-        <div className="flex items-center justify-between px-6 py-4 text-xl rounded-full max-w-sm text-black font-bold bg-gradient-to-r from-orange-400 to-indigo-400 w-full">
+        <div className="flex items-center justify-between px-6 py-4 text-xl rounded-full max-w-sm text-black font-bold bg-gradient-to-r hover:bg-gradient-to-l transition-all duration-300 cursor-pointer from-orange-400 to-indigo-600 w-full">
           <p>Explore Marketplace</p>
           <div className="p-3 bg-yellow-200 rounded-full font-extrabold">
             <div className="p-1 border-2 rounded-full border-black">
-              <AiOutlineRetweet size={20} />
+              <AiOutlineRetweet size={16} />
             </div>
           </div>
         </div>
